48646809c7f95d239a3528061597ad56f1ed3ff4
Pindosaputra123 <pindoajja864@gmail.com>
2023-10-14 19:08:27 +0700

    penambahan file

---
+++ Pertemuan-1/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+     int panjangGaris = 100;
+     for (int i = 0; i< panjangGaris; ++i) {
+          cout << "-";
+     }
+     cout << endl;
+
+     cout<<"Nama : Pindo Saputra Harmanto"<<endl <<endl;
+     cout<<"NPM : 2310631170110"<<endl <<endl;
+     cout<<"Kelas : 1B"<<endl <<endl;
+     cout<<"Cita - Cita : Full Stack Developer"<<endl <<endl;
+     cout<<"Motivasi Masuk Informatika : Lapangan Pekerjaan Yang Luas"<<endl;
+
+     int banyakGaris = 100;
+     for (int i = 0; i< banyakGaris; ++i) {
+          cout << "-";
+     }
+
+}
